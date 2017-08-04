96a28ce000645465fdcfbb0cc9d641b99c0df3d7
Nicolas Bortolotti <nbortolotti@gmail.com>
2017-08-04 22:25:27 +0200

    initial version basic detection functionality

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import logging
+
+import cStringIO
+import urllib
+
 import os
 import sys
 
 import numpy as np
 import tensorflow as tf
+from PIL import Image
 from gcloud import storage
 
-from flask import Flask
-
+from flask import Flask, send_file, render_template
+from flask import jsonify
 
 app = Flask(__name__)
 
 
-
-@app.route('/')
-def hello():
-
-    m = 50
-    n = 1
-
+@app.route('/<number>')
+def tensor_regression(number):
     train_X = np.array([
         [2.0658746e+00],
         [2.3684087e+00],
@@ -73,66 +73,8 @@ def hello():
     ]
     ).astype('float32')
 
-    train_Y = np.array([
-        [7.7918926e-01],
-        [9.1596757e-01],
-        [9.0538354e-01],
-        [9.0566138e-01],
-        [9.3898890e-01],
-        [9.6684740e-01],
-        [9.6436824e-01],
-        [9.1445939e-01],
-        [9.3933944e-01],
-        [9.6074971e-01],
-        [8.9837094e-01],
-        [9.1209739e-01],
-        [9.4238499e-01],
-        [9.6624578e-01],
-        [1.0526500e+00],
-        [1.0143791e+00],
-        [9.5969426e-01],
-        [9.6853716e-01],
-        [1.0766065e+00],
-        [1.1454978e+00],
-        [1.0340625e+00],
-        [1.0070009e+00],
-        [9.6683648e-01],
-        [1.0895919e+00],
-        [1.0634462e+00],
-        [1.1237239e+00],
-        [1.0323374e+00],
-        [1.0874452e+00],
-        [1.0702988e+00],
-        [1.1606493e+00],
-        [1.0778037e+00],
-        [1.1069758e+00],
-        [1.0971875e+00],
-        [1.1648603e+00],
-        [1.1411796e+00],
-        [1.0844156e+00],
-        [1.1252493e+00],
-        [1.1168341e+00],
-        [1.1970789e+00],
-        [1.2069462e+00],
-        [1.1251046e+00],
-        [1.1235672e+00],
-        [1.2132829e+00],
-        [1.2522652e+00],
-        [1.2497065e+00],
-        [1.1799706e+00],
-        [1.1897299e+00],
-        [1.3029934e+00],
-        [1.2601134e+00],
-        [1.2562267e+00]
-
-    ]
-    ).astype('float32')
-
-
     mean = np.mean(train_X, axis=0)
     std = np.std(train_X, axis=0)
-
-    train_X_2 = np.nan_to_num((train_X - mean) / std)
 
     # a = tf.placeholder("float")
     # b = tf.placeholder("float")
@@ -151,73 +93,167 @@ def hello():
     #     #return result
     # inputs
 
-    X = tf.placeholder(tf.float32, [m, n])
-    Y = tf.placeholder(tf.float32, [m, 1])
+    # X = tf.placeholder(tf.float32, [m, n])
+    # Y = tf.placeholder(tf.float32, [m, 1])
 
     # weight and bias
-    W = tf.Variable(tf.zeros([n, 1], dtype=np.float32), name="weight")
-    b = tf.Variable(tf.zeros([1], dtype=np.float32), name="bias")
+    # W = tf.Variable(tf.zeros([n, 1], dtype=np.float32), name="weight")
+    # b = tf.Variable(tf.zeros([1], dtype=np.float32), name="bias")
 
     # linear model
-    with tf.name_scope("linear_Wx_b") as scope:
-        activation = tf.add(tf.matmul(X, W), b)
+    # with tf.name_scope("linear_Wx_b") as scope:
+    #    activation = tf.add(tf.matmul(X, W), b)
 
     # cost
-    with tf.name_scope("cost") as scope:
-        cost = tf.reduce_sum(tf.square(activation - Y)) / (2 * m)
-        tf.summary.scalar("cost", cost)
+    # with tf.name_scope("cost") as scope:
+    #    cost = tf.reduce_sum(tf.square(activation - Y)) / (2 * m)
+    #    tf.summary.scalar("cost", cost)
 
     # train
-    with tf.name_scope("train") as scope:
-        optimizer = tf.train.GradientDescentOptimizer(0.07).minimize(cost)
+    # with tf.name_scope("train") as scope:
+    #    optimizer = tf.train.GradientDescentOptimizer(0.07).minimize(cost)
 
-    #saver = tf.train.Saver()
+    # saver = tf.train.Saver()
 
     # tensorflow session
     with tf.Session() as sess:
 
-        #tf.initialize_all_variables().run()
-
-
-
         client = storage.Client('lookthiscode-521')
         bucket = client.bucket('models_lookthiscode')
-        blob = bucket.blob('my-model.ckpt.meta')
 
-        with open('my-model.ckpt.meta', 'w') as f:
-            blob.download_to_file(f)
+        meta_file = 'my-model.ckpt.meta'
+        if not os.path.isfile(meta_file):
+            blob = bucket.blob(meta_file)
+            with open(meta_file, 'w') as f:
+                blob.download_to_file(f)
 
-        blob1 = bucket.blob('checkpoint')
-        with open('checkpoint', 'w') as f:
-            blob1.download_to_file(f)
+        check_file = 'checkpoint'
+        if not os.path.isfile(check_file):
+            blob1 = bucket.blob(check_file)
+            with open(check_file, 'w') as f:
+                blob1.download_to_file(f)
 
-        blob2 = bucket.blob('my-model.ckpt.index')
-        with open('my-model.ckpt.index', 'w') as f:
-            blob2.download_to_file(f)
+        index_file = 'my-model.ckpt.index'
+        if not os.path.isfile(index_file):
+            blob2 = bucket.blob(index_file)
+            with open(index_file, 'w') as f:
+                blob2.download_to_file(f)
 
-        blob3 = bucket.blob('my-model.ckpt.data-00000-of-00001')
-        with open('my-model.ckpt.data-00000-of-00001', 'w') as f:
-            blob3.download_to_file(f)
-
-        #saver.restore(sess, "/tmp/my-model.meta")
+        data_file = 'my-model.ckpt.data-00000-of-00001'
+        if not os.path.isfile(data_file):
+            blob3 = bucket.blob(data_file)
+            with open(data_file, 'w') as f:
+                blob3.download_to_file(f)
 
         saver = tf.train.import_meta_graph('my-model.ckpt.meta')
         saver.restore(sess, tf.train.latest_checkpoint('./'))
 
-        init = tf.global_variables_initializer()
-        sess.run(init)
-
-        training_cost = sess.run(cost, feed_dict={X: np.asarray(train_X_2), Y: np.asarray(train_Y)})
-
-        predict_X_2 = np.array([7], dtype=np.float32).reshape([1, 1])
-
-
+        predict_X_2 = np.array([float(number)], dtype=np.float32).reshape([1, 1])
         predict_X_2 = (predict_X_2 - mean) / std
-        logging.info(predict_X_2)
-        predict_Y_2 = tf.add(tf.matmul(predict_X_2, W), b)
-        logging.info(predict_Y_2)
 
+        graph = tf.get_default_graph()
+        W = graph.get_tensor_by_name("weight:0")
+        b = graph.get_tensor_by_name("bias:0")
+
+        predict_Y_2 = tf.add(tf.matmul(predict_X_2, W), b)
         return str(sess.run(predict_Y_2))
+
+
+sys.path.append("..")
+
+from utils import label_map_util
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+# MODEL_NAME = 'faster_rcnn_inception_resnet_v2_atrous_coco_11_06_2017'
+MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
+
+MODEL_FILE = MODEL_NAME + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+NUM_CLASSES = 90
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+
+def detect_alert(boxes, classes, scores, category_index, max_boxes_to_draw=20,
+                 min_score_thresh=.5,
+                 ):
+    r = []
+    for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+        if scores is None or scores[i] > min_score_thresh:
+            test1 = None
+            test2 = None
+
+            if category_index[classes[i]]['name']:
+                test1 = category_index[classes[i]]['name']
+                test2 = int(100 * scores[i])
+
+            line = {}
+            line[test1] = test2
+            r.append(line)
+
+    return r
+
+
+def detect_objects(image_np, sess, detection_graph):
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Actual detection.
+    (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    alert_array = detect_alert(np.squeeze(boxes), np.squeeze(classes).astype(np.int32), np.squeeze(scores),
+                               category_index)
+
+    return alert_array
+
+
+def load_image_into_numpy_array(image):
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape(
+        (im_height, im_width, 3)).astype(np.uint8)
+
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+
+def process_image(image):
+    with detection_graph.as_default():
+        with tf.Session(graph=detection_graph) as sess:
+            alert_array = detect_objects(image, sess, detection_graph)
+            return alert_array
+
+
+@app.route('/photo/<path:photo_url>')
+def tensor_photo(photo_url):
+    url = photo_url
+    file = cStringIO.StringIO(urllib.urlopen(photo_url).read())
+    img = Image.open(file)
+
+    if img:
+        list_elements = process_image(img)
+        list = str(list_elements)
+        return render_template('index.html', **locals())
+
 
 
 @app.errorhandler(500)
